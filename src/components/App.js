d8bec7ab49db53b1bf013e1499074e148ce1cec2
Vina Compson <vv.compson@icloud.com>
2018-10-31 11:30:06 +1100

    add simple header

--- src/components/App.js
+++ src/components/App.js
@@ -78,7 +78,10 @@ class App extends Component {
     return (
       <div className='tc'>
           <h1 className='f1'> {USERNAME.toUpperCase()} 's Fornite Stats </h1>
-          <h3 className='tc'>LIFETIME STATS =====> Wins: {lifetimeWins} Kills: {lifetimeKills} K/D: {lifetimekd}</h3>
+          <div className='gamertag-header'>
+            <h3 className='tc'>LIFETIME STAT</h3>
+            <h3><span className="label">Wins: {lifetimeWins} </span><span className="label">Kills: {lifetimeKills} </span><span className="label">K/D: {lifetimekd}</span></h3>
+          </div>
           <div className="dt-ns dt--fixed-ns">
             <div className="dtc-ns tc pv4 bg-black-10">
               <h3 className="bg-light-purple light-blue pa3 ma3" >SOLOS</h3>
